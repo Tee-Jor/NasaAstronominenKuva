a2f76faea9921a458b66527dbcc400a2dfd9c1bb
Teemu Jormanainen <jormanainen.teemu@gmail.com>
2025-09-25 09:22:55 +0300

    UI
    UI

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tervetuloa Nasan päivän astronomiseen kuvaan!");
+
+        while (true)
+        {
+            Console.WriteLine("1. Lataa tämän tai eilisen päivän astronomisen kuvan?\n2. Ladata valitun päivän astronomisen kuvan?\n.3. Ladata satunnaisen päivän astronomisen kuvan?\n4. Poistua?");
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+            }
+        }
     }
 }
